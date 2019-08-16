91a6fa55258d157dfa9d72a37d0c74e820955b63
Burenkovuser <nb2207@yandex.ru>
2019-08-16 10:37:16 +0700

    Initial Commit

---
+++ Sections/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sections
+//
+//  Created by Vasilii on 16/08/2019.
+//  Copyright © 2019 Vasilii Burenkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
